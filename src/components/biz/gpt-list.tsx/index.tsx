53bf496cfee21c6e2082a7d1cdfe50443f3cf7df
root <root@localhost.localdomain>
2023-06-18 15:17:24 +0000

    feat: url

--- src/components/biz/gpt-list.tsx/index.tsx
+++ src/components/biz/gpt-list.tsx/index.tsx
@@ -1,7 +1,13 @@
 const GptList =()=>{
     return (
-        <>
-         <div>hello</div>
-        </>
+        <section>
+            <h2 className="">
+                免费ChatGPT网站
+            </h2>
+            <p><a href="https://chat.jinshutuan.com/#/chat/1687100551376" target="_blank">jinshutuan</a></p>
+        </section>
+        
     )
 }
+
+export default GptList
