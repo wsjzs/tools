b3cd9d606d050791a898db6a2242eb2c72c5d4f1
marks <wsjzs@hotmail.com>
2023-06-25 10:54:37 +0800

    feat: nav title

--- src/components/biz/gpt-list/index.tsx
+++ src/components/biz/gpt-list/index.tsx
@@ -1,7 +1,7 @@
 const GptList = () => (
   <section>
     <h2 className="">
-                免费ChatGPT网站
+        免费的ChatGPT网站资源
     </h2>
     <p><a href="https://chat.jinshutuan.com/#/chat/1687100551376" target="_blank">jinshutuan</a></p>
   </section>
