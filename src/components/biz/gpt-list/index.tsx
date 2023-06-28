f3019f205f8de87b4a05bc94ff588334248b877c
marks <wsjzs@hotmail.com>
2023-06-28 15:02:40 +0800

    feat: add site

--- src/components/biz/gpt-list/index.tsx
+++ src/components/biz/gpt-list/index.tsx
@@ -1,18 +1,30 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-const list = [{
-  icon: 'https://i2.mjj.rip/2023/06/28/78469fb6eca51e9c794bb02dbc32aa71.png',
-  url: 'https://chat.jinshutuan.com/#/chat/1687100551376',
-  title: 'jinshutuan',
-}]
+const list = [
+  {
+    icon: 'https://i2.mjj.rip/2023/06/28/78469fb6eca51e9c794bb02dbc32aa71.png',
+    url: 'https://chat.jinshutuan.com/#/chat/1687100551376',
+    title: 'jinshutuan',
+  },
+  {
+    icon: 'https://i2.mjj.rip/2023/06/28/78469fb6eca51e9c794bb02dbc32aa71.png',
+    url: 'https://ai.usesless.com',
+    title: '小莓用AI',
+  },
+  {
+    icon: 'https://i2.mjj.rip/2023/06/28/2df123d6fd81e29192d0e4ccb94fda25.jpeg',
+    url: 'https://chat.scorpii.net/',
+    title: 'zchat（免费，靠广告活着）',
+  },
+]
 
 const GptList = () => (
   <section className='max-w-lg  w-full mx-auto mt-3'>
     <ul className='w-full'>
       {
         list.map(item => (
-          <li key={item.url} className='w-full'>
+          <li key={item.url} className='w-full mt-2'>
             <div className="flex justify-between items-center bg-neutral-50 dark:bg-neutral-800  p-4 rounded-lg">
               <div className='flex items-center'>
                 <div className='relative w-10 h-10 rounded-lg overflow-hidden dark:bg-slate-100 '>
