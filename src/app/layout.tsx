5b87f9a48c884d3c333b5636a607acfc74a35fc7
marks <wsjzs@hotmail.com>
2023-08-02 17:29:14 +0800

    feat: referrer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,16 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Baidu from '@/components/baidu'
 import { Footer } from '@/components/layout/footer/footer'
+import { Metadata } from 'next'
 import Header from './header'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata:Metadata = {
   title: 'GPT whatfa',
   description: '免费ChatGPT,free gpt',
+  // @ts-ignore
+  referrer: 'unsafe-url',
 }
 
 export default function RootLayout({
